61c1f1f20c8e367cbb5a57bba2a7ef688becaa84
PC-MASTERRACE\damie <damiens94160@gmail.com>
2024-03-20 10:46:00 +0100

    fin exercice 9

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const app = express()
-
+const express = require('express');
+const app = express();
 const db = require("./models/index.js");
+const router = require("./routes/index.js");
+//Ajout des routes
+
+app.use("/api", router);
+
 db.sequelize
 .authenticate()
 .then(() => console.log("Database connected ..."))
 .catch((err) => console.log(err));
+
 
 module.exports = app;
